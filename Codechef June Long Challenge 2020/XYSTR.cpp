3a79f376555e120c0b867dfaf18bd35b5639bf50
MUKUL SHARMA <44135841+mukul9897@users.noreply.github.com>
2020-06-15 16:13:15 +0530

    Accepted(100)

--- Codechef June Long Challenge 2020/XYSTR.cpp
+++ Codechef June Long Challenge 2020/XYSTR.cpp
@@ -1,33 +1,14 @@
 #include<bits/stdc++.h>
 #define ll long long int
 #define ul unsigned long long int
-#define va vector<long long int> a 
-#define vb vector<long long int> b
 const ll M=1e9+7;
 const ll inf=1e18+5;
 using namespace std;
-void fast(){cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);}
-ll bs(ll arr[], ll l, ll r, ll x){ 
-	ll mid=0;
-    if (r >= l){
-		if (arr[mid] == x) return mid; 
-        if (arr[mid] > x) return bs(arr, l, mid - 1, x); 
-        return bs(arr, mid + 1, r, x); 
-    }
-    return -1;
+void fast(){
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(0);
 }
-ll power(ll x,ul y,ll p){
-	ll res = 1;
-	x = x % p;
-	if(x == 0) return 0;
-	while (y > 0){
-		if (y & 1) res = (res*x) % p;
-		y = y>>1;
-		x = (x*x) % p;
-		}
-	return res;
-}
-
 
 void solve(){
 	ll t;
@@ -38,7 +19,10 @@ void solve(){
 		cin>>s;
 		ll l=s.length(),count=0;
 		for(ll i=0;i<l-1;i++){
-			if(s[i]!=s[i+1]){ count++; i++;}
+			if(s[i]!=s[i+1]){ 
+				count++; 
+				i++;
+			}
 		}
 		cout<<count<<"\n";
 	}
